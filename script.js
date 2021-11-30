0c3a9f4f5a11be6f829bb3db5f22550000c15483
Oliveiralsdo <oliveira.lsdo@gmail.com>
2021-11-30 18:57:17 -0300

    Feature reinicia o jogo quando acaba

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ let position = 0;
 function handleKeyUp(event) {
     if (event.keyCode === 32) {
         if (!isJumping) {
-           jump(); 
-        } 
+            jump();
+        }
     }
 }
 
@@ -19,57 +19,59 @@ function jump() {
         if (position >= 150) {
             clearInterval(upInterval);
 
-        // Descendo
-        let downInterval = setInterval(() => {
-            if (position <= 0) {
-                clearInterval(downInterval);
-                isJumping = false; 
-            } else {
-                position -= 20;
-                dino.style.bottom = position + 'px';
-            }
-        } , 20);
-    }   
+            // Descendo
+            let downInterval = setInterval(() => {
+                if (position <= 0) {
+                    clearInterval(downInterval);
+                    isJumping = false;
+                } else {
+                    position -= 20;
+                    dino.style.bottom = position + 'px';
+                }
+            }, 20);
+        }
         else {
-        // Subindo
+            // Subindo
             position += 20;
             dino.style.bottom = position + 'px';
         }
     }, 20);
 }
-      
+
 
 
 function createCactus() {
     const cactus = document.createElement('div');
-    let cactusPosition = 1000; 
+    let cactusPosition = 1000;
     let randomTime = Math.random() * 3000;
 
     cactus.classList.add('cactus');
     cactus.style.left = 1000 + 'px'
-    background.appendChild(cactus);    
-    
-    let leftInterval = setInterval(() =>{  
+    background.appendChild(cactus);
+
+    let leftInterval = setInterval(() => {
         // cactusPosition -= 10;
         // cactus.style.left = cactusPosition + 'px';
-        
+
         if (cactusPosition < -60) {
             clearInterval(leftInterval);
             background.removeChild(cactus);
-
-        } else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+        }
+        else if (cactusPosition > 0 && cactusPosition < 60 && position < 60) {
             // Game over
             clearInterval(leftInterval)
-            document.body.innerHTML = '<h1 class="game-over">Fim de Jogo !</h1>';
+            // document.body.innerHTML = '<h1 class="game-over">Fim de Jogo !</h1>';
+            alert('Fim de Jogo!')
+            location.reload()
         } else {
             cactusPosition -= 10;
             cactus.style.left = cactusPosition + 'px';
         }
-    },20);
+    }, 20);
 
     setTimeout(createCactus, randomTime);
 }
-    
-     
+
+
 createCactus();
 document.addEventListener('keyup', handleKeyUp);
